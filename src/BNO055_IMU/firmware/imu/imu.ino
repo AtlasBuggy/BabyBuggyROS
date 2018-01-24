33b068a51faba78457ac8db8ea067f95018a7481
Xinyu Guan <shguan10@gmail.com>
2018-01-23 23:39:14 -0500

    float32

--- src/BNO055_IMU/firmware/imu/imu.ino
+++ src/BNO055_IMU/firmware/imu/imu.ino
@@ -1,12 +1,14 @@
 #include <ros.h>
 #include <sensor_msgs/Imu.h>
+#include <std_msgs/Float32.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
 ros::NodeHandle nh;
 
-sensor_msgs::Imu imu_msg;
-ros::Publisher chatter("/imu",&imu_msg);
+//sensor_msgs::Imu imu_msg;
+std_msgs::Float32 message;
+ros::Publisher chatter("/imu",&message);
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
@@ -18,25 +20,57 @@ void setup(){
 void loop(){
   //Quaternion
   imu::Quaternion quat = bno.getQuat();
-  imu_msg.Quaternion.x = quat.x();
-  imu_msg.Quaternion.y = quat.y();
-  imu_msg.Quaternion.z = quat.z();
-  imu_msg.Quaternion.w = quat.w();
-
-  //angular velocity
-  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  imu_msg.angular_velocity.x = gyro.x();
-  imu_msg.angular_velocity.y = gyro.y();
-  imu_msg.angular_velocity.z = gyro.z();
-
-  //linear acceleration
-  imu::Vector<3> linaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  imu_msg.linear_acceleration.x = linaccel.x();
-  imu_msg.linear_acceleration.y = linaccel.y();
-  imu_msg.linear_acceleration.z = linaccel.z();
-  
+  message.data = quat.x();
   chatter.publish(&imu_msg);
   nh.spinOnce();
   delay(100);
+
+  message.data = quat.y();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+
+  message.data = quat.z();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+
+  message.data = quat.w();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+
+  //angular velocity
+  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  message.data = gyro.x();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+
+  message.data = gyro.y();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+
+  message.data = gyro.z();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+
+  //linear acceleration
+  imu::Vector<3> linaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  message.data = linaccel.x();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+  message.data = linaccel.y();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+  message.data = linaccel.z();
+  chatter.publish(&imu_msg);
+  nh.spinOnce();
+  delay(100);
+  
 }
-	
+  
