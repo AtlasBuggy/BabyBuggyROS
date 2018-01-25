bd883a303b0e8a3781a875698c7e93271e97d69b
Blair Chen <unbrace3@gmail.com>
2018-01-25 18:24:33 -0500

    Fixed IMU publishing (but still doesn't work) code should be correct, but doesn't work with our Arduino

--- src/firmware/BNO055_IMU/firmware/imu/imu.ino
+++ src/firmware/BNO055_IMU/firmware/imu/imu.ino
@@ -1,76 +1,58 @@
 #include <ros.h>
 #include <sensor_msgs/Imu.h>
-#include <std_msgs/Float32.h>
+#include <std_msgs/Float32MultiArray.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
 ros::NodeHandle nh;
 
 //sensor_msgs::Imu imu_msg;
-std_msgs::Float32 message;
+std_msgs::Float32MultiArray message;
 ros::Publisher chatter("/imu",&message);
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
 void setup(){
   nh.initNode();
+  message.layout.dim = (std_msgs::MultiArrayDimension *)
+  malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
+  message.layout.dim[0].size = 10;
+  message.layout.dim[0].stride = 1*10;
+  message.layout.data_offset = 0;
+  message.layout.dim_length = 1;
+  message.data_length = 10;
+  message.data = (float *)malloc(sizeof(float)*10);
   nh.advertise(chatter);
 }
 
 void loop(){
+  
   //Quaternion
   imu::Quaternion quat = bno.getQuat();
-  message.data = quat.x();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[0] = quat.x();
 
-  message.data = quat.y();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[1] = quat.y();
 
-  message.data = quat.z();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[2] = quat.z();
 
-  message.data = quat.w();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[3] = quat.w();
 
   //angular velocity
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  message.data = gyro.x();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[4] = gyro.x();
 
-  message.data = gyro.y();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[5] = gyro.y();
 
-  message.data = gyro.z();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
+  message.data[6] = gyro.z();
 
   //linear acceleration
   imu::Vector<3> linaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  message.data = linaccel.x();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
-  message.data = linaccel.y();
-  chatter.publish(&imu_msg);
-  //nh.spinOnce();
-  //delay(100);
-  message.data = linaccel.z();
-  chatter.publish(&imu_msg);
+  message.data[7] = linaccel.x();
+
+  message.data[8] = linaccel.y();
+
+  message.data[9] = linaccel.z();
+  chatter.publish(&message);
   nh.spinOnce();
-  delay(100);
-  
 }
   
