225aca91d652afbe8a429033c9ad67727a24b9e4
Blair Chen <unbrace3@gmail.com>
2018-01-26 18:24:37 -0500

    Modified IMU according to handle serial (still doesn't work) Switched IMU model to Naboris model TODO: put code on computer to handle serial, get platform.io to compile new code

--- src/firmware/BNO055_IMU/firmware/imu/imu.ino
+++ src/firmware/BNO055_IMU/firmware/imu/imu.ino
@@ -1,58 +1,47 @@
-#include <ros.h>
-#include <sensor_msgs/Imu.h>
-#include <std_msgs/Float32MultiArray.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
+#include <BNO055_Naboris.h>
+#include <SerialManager.h>
 
-ros::NodeHandle nh;
+SerialManager manager;
 
-//sensor_msgs::Imu imu_msg;
-std_msgs::Float32MultiArray message;
-ros::Publisher chatter("/imu",&message);
+/* ------------------------ *
+ * General global variables *
+ * ------------------------ */
 
-Adafruit_BNO055 bno = Adafruit_BNO055();
+void setup() {
+    manager.begin();
 
-void setup(){
-  nh.initNode();
-  message.layout.dim = (std_msgs::MultiArrayDimension *)
-  malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
-  message.layout.dim[0].size = 10;
-  message.layout.dim[0].stride = 1*10;
-  message.layout.data_offset = 0;
-  message.layout.dim_length = 1;
-  message.data_length = 10;
-  message.data = (float *)malloc(sizeof(float)*10);
-  nh.advertise(chatter);
+    Serial.print("hello!\n");
+
+    initIMU();
+
+    Serial.print("ready!\n");
 }
 
-void loop(){
-  
-  //Quaternion
-  imu::Quaternion quat = bno.getQuat();
-  message.data[0] = quat.x();
+void loop()
+{
+    if (manager.available())
+    {
+        int status = manager.readSerial();
 
-  message.data[1] = quat.y();
+        String command = manager.getCommand();
 
-  message.data[2] = quat.z();
+        if (status == 2)  // start event
+        {
 
-  message.data[3] = quat.w();
+        }
+        else if (status == 1)  // stop event
+        {
 
-  //angular velocity
-  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  message.data[4] = gyro.x();
+        }
+        else if (status == 0)  // user command
+        {
+            
+        }
+    }
 
-  message.data[5] = gyro.y();
-
-  message.data[6] = gyro.z();
-
-  //linear acceleration
-  imu::Vector<3> linaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  message.data[7] = linaccel.x();
-
-  message.data[8] = linaccel.y();
-
-  message.data[9] = linaccel.z();
-  chatter.publish(&message);
-  nh.spinOnce();
+    if (!manager.isPaused()) {
+        updateIMU();
+        // 100Hz update rate for imu
+        delay(10);
+    }
 }
-  
