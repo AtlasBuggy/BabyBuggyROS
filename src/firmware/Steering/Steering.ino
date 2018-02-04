f5c8da46adec39c761d1978967bc3e9f604714c8
Blair Chen <unbrace3@gmail.com>
2018-02-04 15:47:39 -0500

    Working Steering working arduino steering node that subscribes to /steering and will steer to lastest message published; 400 is center, lower than that is to the left, and greater is to the right

--- src/firmware/Steering/Steering.ino
+++ src/firmware/Steering/Steering.ino
@@ -2,6 +2,9 @@
 #include <ros.h>
 #include <std_msgs/Int32.h>
 
+// 0 is left
+// 1023 is right
+// 400 is center
 
 #define PWM_PIN A2
 #define DIR_PIN 4
@@ -10,12 +13,15 @@
 #define A_PIN A1
 
 ros::NodeHandle nh;
+
 std_msgs::Int32 int_msg;
 int goal;
 
-void steer(const std_msgs::Int32::ConstPtr& msg){
+void steer(const std_msgs::Int32& msg){
   goal = msg.data;
 }
+
+ros::Subscriber<std_msgs::Int32> steeringWheel("steering", steer);
 
 void setup() {
 
@@ -26,23 +32,24 @@ void setup() {
 
   digitalWrite(BRK_PIN, LOW);
   goal = analogRead(A_PIN);
+  goal = 400;
   analogWrite(PWM_PIN, 0);
-
+  
   nh.initNode();
-  ros::Subscriber steeringWheel = nh.subscribe("steering", 10, steer);
-  ros::spin();
+  nh.subscribe(steeringWheel);
 }
 
 void loop() {
+  nh.spinOnce();
   int current = analogRead(A_PIN);
-  if (current - goal < 10 && current - goal > -10) {
+  if (current - goal < 5 && current - goal > -5) {
     analogWrite(PWM_PIN, 0);
   } else {
     if (current < goal) {
-      digitalWrite(DIR_PIN, HIGH);
+      digitalWrite(DIR_PIN, LOW);
       analogWrite(PWM_PIN, 255);
     } else {
-      digitalWrite(DIR_PIN, LOW);
+      digitalWrite(DIR_PIN, HIGH);
       analogWrite(PWM_PIN, 255);
     }
   }
