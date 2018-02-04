99c8cfeb2867ab5330e945fc0a95c78540644251
Blair Chen <unbrace3@gmail.com>
2018-02-04 14:44:57 -0500

    Untested Arduino Steering Node Steering node using tested steering commands, will perhaps need fine tuning

--- src/firmware/Steering/Steering.ino
+++ src/firmware/Steering/Steering.ino
@@ -2,37 +2,48 @@
 #include <ros.h>
 #include <std_msgs/Int32.h>
 
-#define motorPin1 D5
-#define motorPin2 D4
 
-#define potPin A0
+#define PWM_PIN A2
+#define DIR_PIN 4
+#define BRK_PIN 3
+
+#define A_PIN A1
 
 ros::NodeHandle nh;
 std_msgs::Int32 int_msg;
+int goal;
 
 void steer(const std_msgs::Int32::ConstPtr& msg){
-  int requestVal = msg.data;
-  int currVal = analogRead(analogPin);
-  while(((int diff=requestVal-currVal)*diff)<25){
-    if(diff>0){
-      digitalWrite(motorPin1,1);
-      digitalWrite(motorPin2,0);
-    }else{
-      digitalWrite(motorPin1,0);
-      digitalWrite(motorPin2,1);
-    }
-    delay(10);
-  }
+  goal = msg.data;
 }
 
 void setup() {
-  pinMode(potPin, INPUT);
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
+
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(BRK_PIN, OUTPUT);
+  pinMode(A_PIN, INPUT);
+
+  digitalWrite(BRK_PIN, LOW);
+  goal = analogRead(A_PIN);
+  analogWrite(PWM_PIN, 0);
 
   nh.initNode();
-  ros::Subscriber steeringWheel = nh.subscribe("chatter", 10, steer);
+  ros::Subscriber steeringWheel = nh.subscribe("steering", 10, steer);
   ros::spin();
 }
 
-void loop(){}
+void loop() {
+  int current = analogRead(A_PIN);
+  if (current - goal < 10 && current - goal > -10) {
+    analogWrite(PWM_PIN, 0);
+  } else {
+    if (current < goal) {
+      digitalWrite(DIR_PIN, HIGH);
+      analogWrite(PWM_PIN, 255);
+    } else {
+      digitalWrite(DIR_PIN, LOW);
+      analogWrite(PWM_PIN, 255);
+    }
+  }
+}
