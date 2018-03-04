dfede4f77d35d166819eb5f95fc801d486c57fab
Woz4tetra <woz4tetra@gmail.com>
2018-03-04 12:58:41 -0500

    Updated arduino code to include the braking servo

--- src/firmware/Steering/Steering.ino
+++ src/firmware/Steering/Steering.ino
@@ -5,6 +5,7 @@
 // 4096 right
 
 #include <ros.h>
+#include <Servo.h>
 #include <std_msgs/Int32.h>
 #include "DualVNH5019MotorShield.h"
 
@@ -12,67 +13,84 @@
 
 DualVNH5019MotorShield md;
 ros::NodeHandle nh;
-int requestVal = 400;
+int steering_requested_value = 400;
+
+Servo brake_servo;
 
 void steer(const std_msgs::Int32& msg){
-  requestVal = msg.data;
+    steering_requested_value = msg.data;
 }
 
-ros::Subscriber<std_msgs::Int32> steeringSub ("steering", steer);
+void set_brakes(const std_msgs::Int32& msg)
+{
+    int brake_value = msg.data;
+    if (brake_value < 0) {
+        brake_value = 0;
+    }
+    if (brake_value > 180) {
+        brake_value = 180;
+    }
+    brake_servo.write(brake_value);
+}
+
+ros::Subscriber<std_msgs::Int32> steeringSub("steering", steer);
+ros::Subscriber<std_msgs::Int32> brakeSub("braking", set_brakes);
 
 void turn_left() {
-  md.setM1Speed(400);
+    md.setM1Speed(400);
 }
 
 void turn_right() {
-  md.setM1Speed(-400);
+    md.setM1Speed(-400);
 }
 
 void stop_motor(){
-  md.setM1Speed(0);
+    md.setM1Speed(0);
 }
 
 void setup() {
-  pinMode(POT_PIN, INPUT);
+    pinMode(POT_PIN, INPUT);
+    brake_servo.attach(3);
 
-  md.init();
-  nh.initNode();
-  nh.subscribe(steeringSub);
+    md.init();
+    nh.initNode();
+    nh.subscribe(steeringSub);
+    nh.subscribe(brakeSub);
 }
 
 void loop(){
-  // absolute steering
-  if (requestVal <= 1023) {
-    int diff = requestVal - analogRead(POT_PIN);
+    // absolute steering
+    if (steering_requested_value <= 1023) {
+        int diff = steering_requested_value - analogRead(POT_PIN);
 
-    if(abs(diff) > 5){
-      if(diff > 0){
-        turn_right();
-      }else{
-        turn_left();
-      }
+        if(abs(diff) > 5){
+            if(diff > 0){
+                turn_right();
+            }else{
+                turn_left();
+            }
+        }
+        else{
+            stop_motor();
+        }
     }
-    else{
-      stop_motor();
-    }
-  }
 
-  else if (requestVal == 1500){
-    stop_motor();
-  }
-
-  else {
-  // relative steering
-    if (requestVal == 2048) {
-      turn_left();
-    } else if (requestVal == 4096) {
-      turn_right();
+    else if (steering_requested_value == 1500){
+        stop_motor();
     }
+
     else {
-      stop_motor();
+        // relative steering
+        if (steering_requested_value == 2048) {
+            turn_left();
+        } else if (steering_requested_value == 4096) {
+            turn_right();
+        }
+        else {
+            stop_motor();
+        }
     }
-  }
 
-  delay(10);
-  nh.spinOnce();
+    delay(10);
+    nh.spinOnce();
 }
