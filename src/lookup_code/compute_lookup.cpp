29de0767f08eb96a729655dde1ec34b90ea8d561
unbrace3 <unbrace3@gmail.com>
2018-04-07 12:46:36 -0400

    controller code with table floodfill

--- src/lookup_code/compute_lookup.cpp
+++ src/lookup_code/compute_lookup.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <map>
 #include <math.h>
+#include <algorithm>
 #include <unordered_map>
 #include <vector>
 #include <string>
@@ -12,49 +13,22 @@ using namespace std;
 #define M_PI 3.14159265358979323846
 
 struct Trajectory {
+  bool fail;
   double dtheta;
   double dx;
   double dy;
   Trajectory ()
   {
+    fail = false;
     dtheta = 0.0;
     dx = 0.0;
     dy = 0.0;
   }
 };
 
-int numDigits(int x)
-{
-    if (x >= 10000) {
-        if (x >= 10000000) {
-            if (x >= 100000000) {
-                if (x >= 1000000000)
-                    return 10;
-                return 9;
-            }
-            return 8;
-        }
-        if (x >= 100000) {
-            if (x >= 1000000)
-                return 7;
-            return 6;
-        }
-        return 5;
-    }
-    if (x >= 100) {
-        if (x >= 1000)
-            return 4;
-        return 3;
-    }
-    if (x >= 10)
-        return 2;
-    if (x == 0) 
-      return 0;
-    return 1;
-}
-
 const double w = 0.05236;
 const double x = 0.8;
+const double dtheta_limit = 2 * M_PI / 3;
 
 /* incr: step size, must be less than tf
  * tm: time in between at which buggy stops wheel rotation (sec)
@@ -73,13 +47,19 @@ Trajectory find_rotation(double incr, double tm, double tf, double a, double b) 
     ang = t * w;
     double d = incr * ((2 * a * t) + b);
     mid = d * sin ((ang / 2.0) + ((incr * w) / 4.0)) / x;
+    //cout << mid << endl;
     tr.dx += d * sin (tr.dtheta + mid);
     tr.dy += d * cos (tr.dtheta + mid);
     tr.dtheta += 2 * mid;
+    if (tr.dtheta > dtheta_limit) {
+      tr.fail = true;
+      return tr;
+    }
   }
   for (;t<=tf-tm;t+=incr) {
     double d = incr * ((2 * a * t) + b);
     mid = d * sin ((ang / 2.0) + ((incr * w) / 4.0)) / x;
+    //cout << mid << endl;
     tr.dx += d * sin (tr.dtheta + mid);
     tr.dy += d * cos (tr.dtheta + mid);
     tr.dtheta += 2 * mid;
@@ -91,6 +71,13 @@ Trajectory find_rotation(double incr, double tm, double tf, double a, double b) 
     tr.dx += d * sin (tr.dtheta + mid);
     tr.dy += d * cos (tr.dtheta + mid);
     tr.dtheta += 2 * mid;
+    if (tr.dtheta > dtheta_limit) {
+      tr.fail = true;
+      return tr;
+    }
+  }
+  if (tr.dtheta < 0) {
+    tr.fail = true;
   }
   return tr;
 }
@@ -98,8 +85,8 @@ Trajectory find_rotation(double incr, double tm, double tf, double a, double b) 
 template <typename T>
 
 struct vectorHasher{
-std::size_t operator()(const std::vector<T> &in) const
-{
+  std::size_t operator()(const std::vector<T> &in) const
+  {
     using boost::hash_value;
     using boost::hash_combine;
     // Start with a hash value of 0
@@ -107,26 +94,32 @@ std::size_t operator()(const std::vector<T> &in) const
     T value;
     for (int i=0; i< in.size(); i++)
     {
-        value = static_cast<T>(in[i]);
-        hash_combine(seed, hash_value(value));
+      value = static_cast<T>(in[i]);
+      hash_combine(seed, hash_value(value));
     }
     return seed;
-}
+  }
 };
 
 
 unordered_map<vector<int>, int, vectorHasher< int > > hashTbl;
 
 ofstream dataFile;
+ofstream maxFile;
 const int vector_size = 6;
 const double sig_fig = 2.0;
 vector<int> key;
+vector<int> maxes;
 
-void init_store(char file[]) {
+void init_store(char file[], char file2[]) {
   for (int i = 0; i < vector_size; i++) {
     key.push_back(0);
+    if (i != 0) {
+      maxes.push_back(0);
+    }
   }
   dataFile.open(file, ios::out | ios::binary);
+  maxFile.open(file2, ios::out | ios::binary);
 }
 
 void store(vector<int> key) {
@@ -137,10 +130,21 @@ void store(vector<int> key) {
   dataFile << endl;
 }
 
+void storeMax(vector<int> maxes) {
+  for ( auto i = maxes.begin(); i != maxes.end(); i++ ) {
+    maxFile << *i << " ";
+    //dataFile.write((char*)(&(*i)), sizeof(int));
+  }
+  maxFile << endl;
+}
+
 void end_store() {
   dataFile << -100000;
   dataFile.flush();
   dataFile.close();
+  maxFile << -100000;
+  maxFile.flush();
+  maxFile.close();
 }
 
 int to_degrees(double theta) {
@@ -150,27 +154,39 @@ int to_degrees(double theta) {
 //const double step_size = 0.01;
 int compute_count = 0;
 
-void compute(char file[], double step_size) {
+void compute(char file[],char file2[], double step_size) {
   int count = 0;
-  init_store(file);
-  for (double tf = 0; tf <= 8; tf += step_size)
+  init_store(file,file2);
+  for (double a = -10 + 0.01; a <= 20; a += step_size)
   {
-    for (double v = step_size; v <= 10; v += step_size)
+    for (double v = step_size + 0.01; v <= 10; v += step_size)
     {
-      for (double a = -10; a <= 10; a += step_size)
+      maxes[0] = (int)(a * sig_fig);
+      maxes[1] = (int)(v * sig_fig);
+      for (int i = 2; i < vector_size - 1; i++) {
+        maxes[i] = 0;
+      }
+
+      for (double tf = 0.21; tf <= 8; tf += step_size)
       {
         Trajectory tr = find_rotation(0.05, tf/2, tf, a/2, v);
+        if (tr.fail)
+          break;
         key[0] = (int)(v * sig_fig);
         key[1] = (int)(a * sig_fig);
-        key[2] = ((int)(to_degrees(tr.dtheta))) % 360;
+        key[2] = (int)(to_degrees(tr.dtheta) / 5);
         key[3] = (int)(tr.dx * sig_fig);
         key[4] = (int)(tr.dy * sig_fig);
         key[5] = (int)(tf * sig_fig);
         store(key);
+        maxes[2] = max(maxes[2], key[2]);
+        maxes[3] = max(maxes[3], key[3]);
+        maxes[4] = max(maxes[4], key[4]);
         count++;
       }
+      storeMax(maxes);
     }
-    cout << compute_count << " " << tf << "/8" << endl;
+    cout << count << " " << (a+10) << "/30" << endl;
   }
   end_store();
   cout << count;
@@ -178,8 +194,9 @@ void compute(char file[], double step_size) {
 }
 
 int main() {
-  char file5[] = "04082018.5.bin";
-  compute(file5, 0.05);
+  char file1[] = "04072018.3.bin";
+  char file2[] = "04072018m.3.bin";
+  compute(file1, file2, 0.05);
   //compute(file2, 0.02);
   //compute(file1, 0.01);
   return 0;
--- src/lookup_code/controller.cpp
+++ src/lookup_code/controller.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdio>
 #include <unordered_map>
+#include <map>
 #include <vector>
 #include <string>
 #include <boost/functional/hash.hpp> //need Boost library
@@ -29,12 +30,60 @@ std::size_t operator()(const std::vector<T> &in) const
 
 
 unordered_map<vector<int>, int, vectorHasher< int > > hashTbl;
+map<int,bool> visited;
 
-const char file[] = "04082018.4.bin";
+const char file[] = "04072018.3.bin";
+const char file2[] = "04072018m.3.bin";
 const int size = 6;
+vector<int> key;
+
+int maxSum;
+int dthetaScale;
+int dxScale;
+int dyScale;
+
+bool checkSum(int dtheta, int dx, int dy) {
+  return ((dthetaScale * dtheta) + (dxScale * dx) + (dyScale * dy) < maxSum);
+}
+
+int floodrecurse(int a, int v, int dtheta, int dx, int dy) {
+  key[2] = dtheta;
+  key[3] = dx;
+  key[4] = dy;
+  //cout << dtheta << " " << dx << " " << dy << endl;
+  int access = hashTbl[key];
+  if (access != 0)
+    return access;
+  if (!checkSum(dtheta,dx,dy))
+    return 1000000;
+  //cout << dtheta << " " << dx << " " << dy << endl;
+  int dtp = floodrecurse(a,v,dtheta+1,dx,dy);
+  int dxp = floodrecurse(a,v,dtheta,dx+1,dy);
+  int dyp = floodrecurse(a,v,dtheta,dx,dy+1);
+  hashTbl[key] = min(min(dtp,dxp),dyp);
+  return min(min(dtp,dxp),dyp);
+}
+
+void floodfill(int a, int v, int dtheta, int dx, int dy) {
+  if (visited[100000 * a + v])
+    return;
+  visited[100000 * a + v] = true;
+  key[0] = v;
+  key[1] = a;
+  dthetaScale = dx * dy;
+  dxScale = dtheta * dy;
+  dyScale = dtheta * dx;
+  maxSum = dtheta * dx * dy / 2;
+  for (int dth = 0; checkSum(dth, 0, 0); dth++) {
+    for (int dxi = 0; checkSum(dth, dxi, 0) ; dxi++) {
+      for (int dyi = 0; checkSum(dth, dxi, dyi) ;dyi++) {
+        floodrecurse(a,v,dth,dxi,dyi);
+      }
+    }
+  }
+}
 
 void init() {
-  vector<int> key;
   int retrieve = 0;
   int value;
   ifstream dataFile;
@@ -42,7 +91,8 @@ void init() {
   for (int i = 0; i < size - 1; i++) {
     key.push_back(0);
   }
- 
+
+  //access table
   int count = 0;
   while (retrieve != -100000) {
     for (int i = 0; i < size - 1; i++) {
@@ -53,16 +103,30 @@ void init() {
     dataFile >> value;
     hashTbl[key] = value;
     count++;
-    if (count % 100000 == 0) cout << count << endl;
+    if (count % 1000000 == 0) cout << count << endl;
+  }
+
+  ifstream maxFile;
+  maxFile.open(file2);
+  while (!maxFile.eof()) {
+    int a, v, dtheta, dx, dy;
+    maxFile >> a;
+    maxFile >> v;
+    maxFile >> dtheta;
+    maxFile >> dx;
+    maxFile >> dy;
+    floodfill(a,v,dtheta,dx,dy);
+    cout << a << " " << v << endl;
   }
  
   dataFile.close();
+  maxFile.close();
 }
 
 int main() {
   init();
   vector<int> key;
-  for (int i = 0; i < 1000; i++)
+  /*for (int i = 0; i < 1000; i++)
   {
     key.push_back(1995);
     key.push_back(230);
@@ -70,6 +134,6 @@ int main() {
     key.push_back(4894);
     key.push_back(2508);
     cout << hashTbl[key];
-  }
+  }*/
   return 0;
 }
