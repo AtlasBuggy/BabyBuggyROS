313e5669e561ac6c86970e0182a7cee6c2b8ebbc
Blair Chen <blairc@andrew.cmu.edu>
2018-10-04 17:52:01 -0400

    smooth node as nu as input

--- src/smooth/scripts/smooth.py
+++ src/smooth/scripts/smooth.py
@@ -3,18 +3,20 @@ import rospy
 import sys
 from std_msgs.msg import Float64
 
-k = 0.1
+nu = 0.1
 if (len(sys.argv) < 2):
     print("Usage: smooth.py channel_name")
     raise ValueError('Please include as an argument the channel you want to smooth!')
+if (len(sys.argv) >= 3):
+    nu = float(sys.argv[2])
 channel = sys.argv[1]
-speed = 0.0
+current_data = 0.0
 pub = rospy.Publisher("smooth_" + channel, Float64, queue_size=10)
 
 def smooth_callback(data):
-    diff = data.data - speed
-    speed += k * diff
-    pub.publish(speed)
+    diff = data.data - current_data
+    current_data += nu * diff
+    pub.publish(current_data)
 
 def smoother():
     rospy.init_node('smoother', anonymous=True)
