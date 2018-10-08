0b714308368860c74f717c2c2735b063af5ed42b
Oshadha Gunasekara <ovg@andrew.cmu.edu>
2018-10-08 16:06:05 -0400

    Incorporate 3D rotation into dead reckoning estimator

--- src/dead_reckoning/scripts/dead_reckoning.py
+++ src/dead_reckoning/scripts/dead_reckoning.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import math
+import numpy as np
 
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Pose
@@ -9,7 +10,7 @@ from std_msgs.msg import Float64
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 p = 0.1
-state = [0, 0, 0, 0] # x, y, th, v
+state = [0, 0, 0, 0, 0, 0] # [x, y, z, v, w]
 prev_time = None
 orientation = [0, 0, 0, 1]
 
@@ -18,6 +19,10 @@ odom_pub = None
 def wheel_vel_callback(msg):
     global state
     state[3] = msg.data
+
+def ang_vel_callback(msg):
+    global state
+    state[4] = msg.data
 
 def imu_callback(msg):
     global state, prev_time, orientation
@@ -33,20 +38,45 @@ def imu_callback(msg):
     orientation = [x, y, z, w]
     (roll, pitch, yaw) = euler_from_quaternion(orientation)
 
-    state[2] += (yaw - state[2]) * p # smooth yaw reading
+    yawMatrix = np.matrix([
+                [math.cos(yaw), -math.sin(yaw), 0],
+                [math.sin(yaw), math.cos(yaw), 0],
+                [0, 0, 1]
+                ])
+
+    pitchMatrix = np.matrix([
+                [math.cos(pitch), 0, math.sin(pitch)],
+                [0, 1, 0],
+                [-math.sin(pitch), 0, math.cos(pitch)]
+                ])
+
+    rollMatrix = np.matrix([
+                [1, 0, 0],
+                [0, math.cos(roll), -math.sin(roll)],
+                [0, math.sin(roll), math.cos(roll)]
+                ])
+
+    R = yawMatrix * pitchMatrix * rollMatrix
+    vel_mat = np.matrix([[state[3]], [0], [0]])
+
+    transf_vel = R * vel_mat
+
+    # state[2] += (yaw - state[2]) * p # smooth yaw reading
 
     cur_time = rospy.get_time()
     dt = cur_time - prev_time
     prev_time = cur_time
 
-    state[0] = state[0] + state[3]*math.cos(state[2])*dt
-    state[1] = state[1] + state[3]*math.sin(state[2])*dt
+    state[0] = state[0] + transf_vel[0,0]*dt
+    state[1] = state[1] + transf_vel[1,0]*dt
+    state[2] = state[2] + transf_vel[2,0]*dt
 
 def main():
     global odom_pub, orientation, state
     rospy.init_node('dead_reckoning', anonymous=True)
 
     rospy.Subscriber("wheel_vel", Float64, wheel_vel_callback)
+    rospy.Subscriber("ang_vel", Float64, ang_vel_callback)
     rospy.Subscriber("BNO055", Imu, imu_callback)
 
     odom_pub = rospy.Publisher("dead_reckon_odom", Pose, queue_size=10)
@@ -58,6 +88,7 @@ def main():
 
         msg.position.x = state[0]
         msg.position.y = state[1]
+        msg.position.z = state[2]
 
         msg.orientation.x = orientation[0]
         msg.orientation.y = orientation[1]
