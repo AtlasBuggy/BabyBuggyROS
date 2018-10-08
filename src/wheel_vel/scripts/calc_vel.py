1ace8558c42a3c97ca5820e9c901467065b51784
Oshadha Gunasekara <ovg@andrew.cmu.edu>
2018-10-08 16:06:42 -0400

    Add ang_vel calculation to calc_vel.py

--- src/wheel_vel/scripts/calc_vel.py
+++ src/wheel_vel/scripts/calc_vel.py
@@ -11,32 +11,36 @@ vel_pub = None
 # Constants
 p = 0.1
 d = 0
-enc_tpr = 1024.0;
-enc_radius = 0.030825;
-enc_ticks_to_m = 2.0 * math.pi * enc_radius / enc_tpr;
+enc_tpr = 1024.0
+enc_radius = 0.030825
+enc_ticks_to_m = 2.0 * math.pi * enc_radius / enc_tpr
+wheel_dist = 0.152
 
 # Calculation variables
 left_dist = None
 left_vel = 0
 left_prev_time = 0
 left_prev_err = None
+left_updated = 0
 
 right_dist = None
 right_vel = 0
 right_prev_time = 0
 right_prev_err = None
+right_updated = 0
 
 def enc1_callback(msg):
     global left_dist, left_vel, left_prev_time, left_prev_err
     global enc_ticks_to_m
 
-    if left_dist == None:
-        left_dist = msg.data * enc_ticks_to_m
-        left_prev_time = rospy.get_time()
-    else:
-        cur_dist = msg.data * enc_ticks_to_m
-        cur_time = rospy.get_time()
+    cur_dist = msg.data * enc_ticks_to_m
+    cur_time = rospy.get_time()
 
+    # update left velocity and dist
+    if left_dist == None:
+        left_dist = cur_dist
+        left_prev_time = cur_time
+    else:
         d_dist = cur_dist - left_dist
         d_t = cur_time - left_prev_time
 
@@ -47,7 +51,7 @@ def enc1_callback(msg):
         err = cur_vel - left_vel
 
         if left_prev_err == None:
-            left_prev_err = err;
+            left_prev_err = err
 
         left_vel += p * err + d * ((err - left_prev_err) / d_t)
         left_prev_err = err
@@ -79,19 +83,26 @@ def enc2_callback(msg):
         right_prev_err = err
 
 def main():
-    global vel_pub, left_vel, right_vel
+    global vel_pub, left_vel, right_vel, wheel_dist
     rospy.init_node('velocity_calculator', anonymous=True)
 
     rospy.Subscriber("encoder1_raw", Int64, enc1_callback)
     rospy.Subscriber("encoder2_raw", Int64, enc2_callback)
 
     vel_pub = rospy.Publisher("wheel_vel", Float64, queue_size=10)
+    ang_vel_pub = rospy.Publisher("ang_vel", Float64, queue_size=10)
 
     rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
-        vel = (left_vel + right_vel)/2.0
+        vel = (right_vel + left_vel)/2.0
         vel_pub.publish(vel)
+
+        ang_vel = (right_vel - left_vel)/(wheel_dist)
+        # ang_vel = math.atan2(math.sin(ang_vel), math.cos(ang_vel))
+
+        ang_vel_pub.publish(ang_vel)
+
         rate.sleep()
 
 if __name__ == '__main__':
