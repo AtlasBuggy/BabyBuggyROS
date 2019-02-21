4d453f4a3e737923ceed1fcde4a82c9c336ade44
Oshadha Gunasekara <ovg@andrew.cmu.edu>
2019-02-21 14:03:35 -0500

    removed time dependency in dead reckoning

--- src/dead_reckoning/scripts/dead_reckoning.py
+++ src/dead_reckoning/scripts/dead_reckoning.py
@@ -1,132 +1,125 @@
 #!/usr/bin/env python
 import rospy
+import sys
 import math
+import time
 import numpy as np
 
 import tf2_ros
 
-from sensor_msgs.msg import Imu
+from std_msgs.msg import Float64, Int64
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
-from std_msgs.msg import Float64
+from sensor_msgs.msg import Imu
 
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
-USE_ANG_VEL = True
-USE_IMU_ANG_VEL = False
+# ROS variables
+vel_pub = None
 
-UPDATE_RATE = 20
+# Constants
+p = 0.2
+d = 0
+enc_tpr = 1024.0
+enc_radius = 0.030825
+enc_ticks_to_m = 2.0 * math.pi * enc_radius / enc_tpr
+wheel_dist = 0.152
 
-state = [0, 0, 0, 0, 0] # [x, y, z, v, w]
-ang_velocity = [0, 0, 0] # [wx, wy, wz]
-orientation = [0, 0, 0, 1]    # [x, y, z, w]
-orientation_euler = [0, 0, 0] # [roll, pitch, yaw]
+# Calculation variables
+left_enc = None
+prev_left_enc = 0
+right_enc = None
+prev_right_enc = 0
+imu_orientation = None
+imu_quaternion = None
+state = [0,0,0]
 
-prev_time = None
-ang_vel_gain = 0.1
+def enc1_callback(msg):
+    global right_enc
+    right_enc = msg.data
 
-odom_pub = None
-
-def wheel_vel_callback(msg):
-    global state
-    state[3] = msg.data
-
-def ang_vel_callback(msg):
-    global state
-    state[4] = msg.data
+def enc2_callback(msg):
+    global left_enc
+    left_enc = msg.data
 
 def imu_callback(msg):
-    global state, prev_time, orientation, orientation_euler, ang_velocity
+    global imu_orientation, imu_quaternion
 
-    # ensure we can calculate a dt
-    if prev_time == None:
-        prev_time = rospy.get_time()
-        return
+    x = msg.orientation.x
+    y = msg.orientation.y
+    z = msg.orientation.z
+    w = msg.orientation.w
 
-    # use angular velocity from IMU
-    if ang_velocity[0] == 0:
-        ang_velocity[0] = msg.angular_velocity.x
-        ang_velocity[1] = msg.angular_velocity.y
-        ang_velocity[2] = msg.angular_velocity.z
-    else:
-        ang_velocity[0] = ang_velocity[0] + ang_vel_gain * (msg.angular_velocity.x - ang_velocity[0])
-        ang_velocity[1] = ang_velocity[1] + ang_vel_gain * (msg.angular_velocity.y - ang_velocity[1])
-        ang_velocity[2] = ang_velocity[2] + ang_vel_gain * (msg.angular_velocity.z - ang_velocity[2])
-
-    # get dt using rospy
-    cur_time = rospy.get_time()
-    dt = cur_time - prev_time
-    prev_time = cur_time
-
-    # update orientation and orientation_euler
-    if USE_ANG_VEL:
-        if USE_IMU_ANG_VEL:
-            orientation_euler[0] = orientation_euler[0] + dt * ang_velocity[0]
-            orientation_euler[1] = orientation_euler[1] + dt * ang_velocity[1]
-            orientation_euler[2] = orientation_euler[2] + dt * ang_velocity[2]
-        else:
-            orientation_euler[2] = orientation_euler[2] + dt * state[4]
-
-        (x, y, z, w) = quaternion_from_euler(orientation_euler[0], orientation_euler[1], orientation_euler[2])
-        orientation = [x, y, z, w]
-    else:
-        x = msg.orientation.x
-        y = msg.orientation.y
-        z = msg.orientation.z
-        w = msg.orientation.w
-        orientation = [x, y, z, w]
-        (roll, pitch, yaw) = euler_from_quaternion(orientation)
-        orientation_euler = [roll, pitch, yaw]
-
-    # use roll, pitch, and yaw to find rotation transform
-    roll = orientation_euler[0]
-    pitch = orientation_euler[1]
-    yaw = orientation_euler[2]
-
-    yawMatrix = np.matrix([
-                [math.cos(yaw), -math.sin(yaw), 0],
-                [math.sin(yaw), math.cos(yaw), 0],
-                [0, 0, 1]
-                ])
-
-    pitchMatrix = np.matrix([
-                [math.cos(pitch), 0, math.sin(pitch)],
-                [0, 1, 0],
-                [-math.sin(pitch), 0, math.cos(pitch)]
-                ])
-
-    rollMatrix = np.matrix([
-                [1, 0, 0],
-                [0, math.cos(roll), -math.sin(roll)],
-                [0, math.sin(roll), math.cos(roll)]
-                ])
-
-    R = yawMatrix * pitchMatrix * rollMatrix
-    vel_mat = np.matrix([[state[3]], [0], [0]])
-
-    # update state based off of rotated velocity
-    transf_vel = R * vel_mat
-    cur_time = rospy.get_time()
-    dt = cur_time - prev_time
-    prev_time = cur_time
-
-    state[0] = state[0] + transf_vel[0,0]*dt
-    state[1] = state[1] + transf_vel[1,0]*dt
-    state[2] = state[2] + transf_vel[2,0]*dt
+    imu_quaternion = [x, y, z, w]
+    (roll, pitch, yaw) = euler_from_quaternion(imu_quaternion)
+    imu_orientation = [roll, pitch, yaw]
 
 def main():
-    global odom_pub, orientation, state
+    global right_enc, prev_right_enc
+    global left_enc, prev_left_enc
+    global imu_orientation, imu_quaternion
+    global state
+
     rospy.init_node('dead_reckoning', anonymous=True)
 
-    rospy.Subscriber("wheel_vel", Float64, wheel_vel_callback)
-    rospy.Subscriber("ang_vel", Float64, ang_vel_callback)
+    rospy.Subscriber("encoder1_raw", Int64, enc1_callback) # right_vel
+    rospy.Subscriber("encoder2_raw", Int64, enc2_callback) # left_vel
     rospy.Subscriber("BNO055", Imu, imu_callback)
 
     odom_pub = rospy.Publisher("odom", Odometry, queue_size=10)
 
-    rate = rospy.Rate(UPDATE_RATE) # 10hz
+    rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
+        if right_enc == None or left_enc == None:
+            continue
+
+        if imu_orientation == None or imu_quaternion == None:
+            continue
+
+        if prev_right_enc == None or prev_left_enc == None:
+            prev_right_enc = right_enc
+            prev_left_enc = left_enc
+
+        right_diff = right_enc - prev_right_enc
+        left_diff = left_enc - prev_left_enc
+
+        avg_diff = (right_diff + left_diff)/2.0
+
+        prev_left_enc = left_enc
+        prev_right_enc = right_enc
+
+        roll = imu_orientation[0]
+        pitch = imu_orientation[1]
+        yaw = imu_orientation[2]
+
+        yawMatrix = np.matrix([
+                    [math.cos(yaw), -math.sin(yaw), 0],
+                    [math.sin(yaw), math.cos(yaw), 0],
+                    [0, 0, 1]
+                    ])
+
+        pitchMatrix = np.matrix([
+                    [math.cos(pitch), 0, math.sin(pitch)],
+                    [0, 1, 0],
+                    [-math.sin(pitch), 0, math.cos(pitch)]
+                    ])
+
+        rollMatrix = np.matrix([
+                    [1, 0, 0],
+                    [0, math.cos(roll), -math.sin(roll)],
+                    [0, math.sin(roll), math.cos(roll)]
+                    ])
+
+        R = yawMatrix * pitchMatrix * rollMatrix
+        vel_mat = np.matrix([[avg_diff], [0], [0]])
+
+        transf_vel = R * vel_mat
+
+        state[0] = state[0] + transf_vel[0,0]*enc_ticks_to_m
+        state[1] = state[1] + transf_vel[1,0]*enc_ticks_to_m
+        state[2] = state[2] + transf_vel[2,0]*enc_ticks_to_m
+
         # send transform message to ROS
         broadcaster = tf2_ros.StaticTransformBroadcaster()
         tf_msg = TransformStamped()
@@ -138,10 +131,10 @@ def main():
         tf_msg.transform.translation.x = state[0]
         tf_msg.transform.translation.y = state[1]
         tf_msg.transform.translation.z = state[2]
-        tf_msg.transform.rotation.x = orientation[0]
-        tf_msg.transform.rotation.y = orientation[1]
-        tf_msg.transform.rotation.z = orientation[2]
-        tf_msg.transform.rotation.w = orientation[3]
+        tf_msg.transform.rotation.x = imu_quaternion[0]
+        tf_msg.transform.rotation.y = imu_quaternion[1]
+        tf_msg.transform.rotation.z = imu_quaternion[2]
+        tf_msg.transform.rotation.w = imu_quaternion[3]
 
         broadcaster.sendTransform(tf_msg)
 
@@ -152,17 +145,15 @@ def main():
         new_msg.child_frame_id = "base_link"
         new_msg.pose.pose.position.x = state[0]
         new_msg.pose.pose.position.y = state[1]
-        # new_msg.pose.pose.position.z = state[2]
         new_msg.pose.pose.position.z = 0
 
-        new_msg.pose.pose.orientation.x = orientation[0]
-        new_msg.pose.pose.orientation.y = orientation[1]
-        new_msg.pose.pose.orientation.z = orientation[2]
-        new_msg.pose.pose.orientation.w = orientation[3]
+        new_msg.pose.pose.orientation.x = imu_quaternion[0]
+        new_msg.pose.pose.orientation.y = imu_quaternion[1]
+        new_msg.pose.pose.orientation.z = imu_quaternion[2]
+        new_msg.pose.pose.orientation.w = imu_quaternion[3]
 
         odom_pub.publish(new_msg)
 
-        # sleep to maintain update rate
         rate.sleep()
 
 if __name__ == '__main__':
--- src/wheel_vel/scripts/calc_vel.py
+++ src/wheel_vel/scripts/calc_vel.py
@@ -2,6 +2,7 @@
 import rospy
 import sys
 import math
+import time
 
 from std_msgs.msg import Float64, Int64
 
@@ -9,7 +10,7 @@ from std_msgs.msg import Float64, Int64
 vel_pub = None
 
 # Constants
-p = 0.1
+p = 0.2
 d = 0
 enc_tpr = 1024.0
 enc_radius = 0.030825
@@ -29,27 +30,32 @@ right_prev_time = 0
 right_prev_err = None
 right_updated = 0
 
+ang_vel = 0
+ang_vel_prev_time = 0
+
 def enc1_callback(msg):
     global right_dist, right_vel, right_prev_time, right_prev_err
     global enc_ticks_to_m
 
     if right_dist == None:
         right_dist = msg.data * enc_ticks_to_m
-        right_prev_time = rospy.get_time()
+        # right_prev_time = rospy.get_time()
+        right_prev_time = time.time()
     else:
         cur_dist = msg.data * enc_ticks_to_m
-        cur_time = rospy.get_time()
+        # cur_time = rospy.get_time()
+        cur_time = time.time()
 
         d_dist = cur_dist - right_dist
         d_t = cur_time - right_prev_time
 
-        right_dist = cur_dist
+        right_dist = d_dist
         right_prev_time = cur_time
 
         cur_vel = d_dist / d_t
         err = cur_vel - right_vel
 
-        right_vel += p * err
+        right_vel = right_vel + p * err
 
 def enc2_callback(msg):
     global left_dist, left_vel, left_prev_time, left_prev_err
@@ -58,28 +64,32 @@ def enc2_callback(msg):
     # update left velocity and dist
     if left_dist == None:
         left_dist = msg.data * enc_ticks_to_m
-        left_prev_time = rospy.get_time()
+        # left_prev_time = rospy.get_time()
+        left_prev_time = time.time()
     else:
         cur_dist = msg.data * enc_ticks_to_m
-        cur_time = rospy.get_time()
+        # cur_time = rospy.get_time()
+        cur_time = time.time()
 
         d_dist = cur_dist - left_dist
         d_t = cur_time - left_prev_time
 
-        left_dist = cur_dist
+        left_dist = d_dist
         left_prev_time = cur_time
 
         cur_vel = d_dist / d_t
         err = cur_vel - left_vel
 
-        left_vel += p * err
+        left_vel = left_vel + p * err
 
 def main():
     global vel_pub, left_vel, right_vel, wheel_dist
+    global ang_vel, left_dist, right_dist, ang_vel_prev_time
+
     rospy.init_node('velocity_calculator', anonymous=True)
 
-    rospy.Subscriber("encoder1_raw", Int64, enc1_callback)
-    rospy.Subscriber("encoder2_raw", Int64, enc2_callback)
+    rospy.Subscriber("encoder1_raw", Int64, enc1_callback) # right_vel
+    rospy.Subscriber("encoder2_raw", Int64, enc2_callback) # left_vel
 
     vel_pub = rospy.Publisher("wheel_vel", Float64, queue_size=10)
     ang_vel_pub = rospy.Publisher("ang_vel", Float64, queue_size=10)
@@ -87,12 +97,22 @@ def main():
     rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
+        # calculate and publish linear velocity
         # vel = (right_vel + left_vel)/2.0
-        vel = right_vel
+        vel = left_vel
         vel_pub.publish(vel)
 
-        ang_vel = (right_vel - left_vel)/(wheel_dist)
-        ang_vel_pub.publish(ang_vel)
+        # calculate and publish angular velocity
+        if (right_dist != None) and (left_dist != None):
+            cur_time = rospy.get_time()
+            d_dist = (right_dist - left_dist)/(wheel_dist)
+            d_t = cur_time - ang_vel_prev_time
+
+            cur_ang_vel = d_dist / d_t
+            ang_vel_prev_time = cur_time
+
+            ang_vel += (cur_ang_vel - ang_vel)*p
+            ang_vel_pub.publish(ang_vel)
 
         rate.sleep()
 
